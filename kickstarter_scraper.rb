ec920ed2b3464d32c4f0768aa6910ccc06924f60
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-16 19:02:13 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,7 +12,7 @@ def create_project_hash
   # description = project.css('p.bbcard_blurb').text
   # location = project.css('span.location-name').text
   # percent_funded = project.css('ul.project-stats')
-  
+
 
   binding.pry
 end
