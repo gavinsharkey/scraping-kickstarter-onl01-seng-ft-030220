a5c6276aeaf37be85a612fb8e11f5c05c3a3dd7f
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-16 18:46:46 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,9 +1,10 @@
 # require libraries/modules here
 require 'nokogiri'
 
-html = File.read('fixtures/kickstarter.html')
-doc = Nokogiri::HTML(html)
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  doc = Nokogiri::HTML(html)
+
 end
