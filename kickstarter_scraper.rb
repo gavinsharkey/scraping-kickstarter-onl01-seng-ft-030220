d325016f479d8d44fe8c696070d2d4f63d827a7c
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-16 19:07:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -6,7 +6,7 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   doc = Nokogiri::HTML(html)
-  
+
   projects = {}
 
   doc.css('li.project.grid_4').each do |project|
@@ -18,7 +18,7 @@ def create_project_hash
       percent_funded: project.css('ul.project-stats li.first.funded strong').text.gsub('%', '').to_i
     }
   end
-  
+
   projects
   binding.pry
 end
